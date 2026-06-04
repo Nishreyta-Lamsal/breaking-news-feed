bddc0060911a421bc567a7709ef027b7532c9c2f
Nishreyta-Lamsal <nishreytal@gmail.com>
2026-06-04 11:28:46 +0545

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const page = () => {
+
+  const [articles, setArticles] = useState([]);
+  const [pendingArticles, setPendingArtciles] = useState([]);
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
